fda2785346827adc103cacac3f6f79489dd48d7e
Mario Aracena <malexisaracena@gmail.com>
2023-06-13 18:42:52 -0400

    Incorporación de bootstrap

--- Entrega3/AppE3/urls.py
+++ Entrega3/AppE3/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from AppE3 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('AppE3/', include('AppE3.urls'))
+    
 ]
