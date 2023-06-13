064e0a0faff6c054c5a22b97a4cc812b4a563ac4
Mario Aracena <malexisaracena@gmail.com>
2023-06-13 01:28:02 -0400

    Creación de clases en Models

--- Entrega3/AppE3/models.py
+++ Entrega3/AppE3/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Alumno(models.Model):
+    NombreCompleto = models.CharField(max_length=25)
+    GradoCinturon = models.IntegerField()
+
+class MaestroTKD(models.Model):
+    NombreCompleto = models.CharField(max_length=25)
+    GradoCinturon = models.IntegerField()
+    Emailmaestro = models.EmailField()
+
+class TutorAlumno(models.Model):
+    Nombretutor = models.CharField(max_length=30)
+    Emailtutor = models.EmailField()
+    Fonotutor =models.BigIntegerField(9)
